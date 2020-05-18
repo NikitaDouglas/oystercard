a0aface4490c9b31e4c6bd97fe86d879875f0d7e
Nikita <latinisnotdead07@gmail.com>
2020-05-18 16:55:38 +0100

    refactored to add descibe block

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,15 +4,17 @@ describe Oystercard do
   subject(:card) {Oystercard.new}
 
   it "allows a user to check the balance of the card" do
-  expect(card.balance).to eq(0)
-end
-
-  it 'allows a user to top up their card' do
-    expect { card.top_up(10) }.to change { card.balance }
+    expect(card.balance).to eq(0)
   end
 
-  it "limits the amount a user can top to £90" do
-    expect { card.top_up(91) }.to raise_error("ERROR: The Oyster Card top-up limit is £90")
-  end
+  describe "#top_up" do
 
+     it 'allows a user to top up their card' do
+       expect { card.top_up(10) }.to change { card.balance }
+     end
+
+     it "limits the amount a user can top to £90" do
+       expect { card.top_up(91) }.to raise_error("ERROR: The Oyster Card top-up limit is £90")
+     end
+   end
 end
